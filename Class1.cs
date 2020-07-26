889da45e5344c55e0917ca97ef990a806956a8ed
vishal-parameswaran <vishalpr1969@gmail.com>
2020-07-27 02:26:40 +0530

    Upload Batch 1

---
+++ Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+namespace FolderSize
+{
+    public class Program
+    {
+        public static string DirectorySize(String DirectoryName)
+        {            
+            if (Directory.Exists(DirectoryName)){
+            DirectoryInfo dInfo = new DirectoryInfo(DirectoryName);
+            bool includeSubDir = true;
+            long totalSize = dInfo.EnumerateFiles()
+                         .Sum(file => file.Length);            
+            if (includeSubDir)
+            {                
+                totalSize += dInfo.EnumerateDirectories()
+                         .Sum(dir => long.Parse(DirectorySize(dir.FullName)));
+            }
+            return totalSize.ToString();}
+            else
+            {
+                return "Error: Incorrect Folder Name";
+            }
+        }
+    }
+}
